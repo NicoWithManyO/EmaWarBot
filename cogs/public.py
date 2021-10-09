4c35761ccc86b9e96b7ea566c3e7653323ad73cc
NicoWithManyO <nicowithmanyo@gmail.com>
2021-10-10 00:06:55 +0200

    addPublicCMD

---
+++ cogs/public.py
@@ -0,0 +1,15 @@
+
+import discord
+from discord.ext import commands
+from discord.ext import tasks
+
+
+
+class Public(commands.Cog):
+    def __init__(self, ewb):
+        self.ewb = ewb
+
+    
+
+def setup(bot):
+    bot.add_cog(Public(bot))
