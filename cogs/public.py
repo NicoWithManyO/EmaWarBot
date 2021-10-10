9a9ff96db997ba7d1e43c12aaa8aefbb12d2d63f
NicoWithManyO <nicowithmanyo@gmail.com>
2021-10-10 08:45:59 +0200

    firstCode

--- cogs/public.py
+++ cogs/public.py
@@ -3,13 +3,49 @@ import discord
 from discord.ext import commands
 from discord.ext import tasks
 
+import helpers.templates_to_publish as templates
 
+bot = discord.Bot()
 
 class Public(commands.Cog):
     def __init__(self, ewb):
         self.ewb = ewb
 
-    
+    @commands.command(aliases = ["infos"])
+    async def info(self, ctx, querry):
+        """Retourne les informations connues sur l'élément donné
+        
+        Fournir un rôle, un membre, pour en avoir les informations
+        - ema.info @membre
+        """
+        role = None
+        member = None
+        querry = querry.replace("<@!","").replace("<@&","").replace(">","")
+        role = discord.utils.get(ctx.guild.roles,id=int(querry))
+        if role:
+            desc = f"{len(role.members)} membre(s) | Mentionable : {role.mentionable}\nCréé le {str(role.created_at)[:10]}\nid:{role.id}"
+            author = f"{type(role)}"
+            title = f"{role}"
+            logo = None
+        else:
+            member = discord.utils.get(ctx.guild.members,id=int(querry))
+            if member:
+                print(member)
+                print(type(member))
+                roles = []
+                bot = ""
+                
+                if member.bot:
+                    bot = emojis.system
+                for x in member.roles:
+                    roles.append(x.name)
+                title = f"{bot} {member.display_name}"
+                logo = member.avatar
+                desc = f"Rôle(s) : {', '.join(roles)}\nDiscord depuis : {str(member.created_at)[:10]}\nMembre ici depuis : {str(member.joined_at)[:10]}\nid:{member.id}"
+                author = f"{type(member)}"
+        # await ctx.send(f"{title} {desc} {author}")
+        await ctx.send(embed = templates.create_std_embed(self, ctx, title = f"{title}", desc = f"{desc}", author = f"{author}", logo = logo))
+
 
 def setup(bot):
     bot.add_cog(Public(bot))
--- config_files/ewb_bot.py
+++ config_files/ewb_bot.py
@@ -1,7 +1,3 @@
-        # super().__init__(description = ewb_config.description,
-        #             intents = ewb_config.intents,
-        #             command_prefix = ewb_config.prefix,
-        #             case_insensitive = ewb_config.case_insensitive)
 
 import discord
 
@@ -9,6 +5,10 @@ import discord
 prefix = ["dev.", "ecup.", "ranking.", "ecl."]
 case_insensitive = True
 intents = discord.Intents.all()
+
+# id options
+signature = "[EmaWarBot] par NicoWithManyO#8020"
+bot_avatar = "https://cdn.discordapp.com/attachments/860198153047113789/860198259732643870/tmp.png"
 description = "EmaWarBot pour E-magine Gaming | NicoWithManyO#8020"
 
 # boot options 
