e8302942fe74a6da24c9d20b961ba99e05f10a9a
NicoWithManyO <nicowithmanyo@gmail.com>
2021-10-26 19:59:26 +0200

    fix

--- cogs/tournaments_sheduled_tasks.py
+++ cogs/tournaments_sheduled_tasks.py
@@ -77,9 +77,9 @@ class TournamentsSheduledTasks(commands.Cog):
                     for x in to_show:
                         await channel.send(embed = x)
                         if team['ewb_New'] == False:
-                            await channel.send(f"> [ewb] L'équipe {team['ewb_NomEquipe']} à déjà participé, après avoir vérifier les données (particulièrement le Roster {team['ewb_Roster']}) vous pouvez ajouter :white_check_mark:, nous validerons dès que possible ...")
+                            await channel.send(f"> [ewb] L'équipe **`{team['ewb_NomEquipe']}`** à déjà participé, après avoir vérifier les données (particulièrement le Roster **`{team['ewb_Roster']}`** et votre tag clan **`{team['ewb_Tag']}`**) vous pouvez ajouter :white_check_mark:, nous validerons dès que possible ...")
                         else:
-                            await channel.send(f"> [ewb] Bienvenue dans le Ranking, merci de vérifier les informations de votre inscription (le roster d'inscription particulièrement {team['ewb_Roster']}, ainsi que le nom de votre équipe {team['ewb_NomEquipe']}) ... **Merci ensuite de prendre contact (dans le #flood ou les rooms #ewb_check) avec le @Staff E-magine**, afin que nous validions définitivement votre inscription !")
+                            await channel.send(f"> [ewb] Bienvenue dans le Ranking, merci de vérifier les informations de votre inscription (le roster d'inscription particulièrement **`{team['ewb_Roster']}`**, ainsi que le nom de votre équipe **`{team['ewb_NomEquipe']}`**, sans oublier le tag du **clan dans le quel vous jouerez le match **`{team['ewb_Tag']}`**) ... **Merci ensuite de prendre contact (dans le #flood ou les rooms #ewb_check) avec le @Staff E-magine**, afin que nous validions définitivement votre inscription !")
                 else:
                     print(f"{tournament} registrations : {len(tournament.registrations_teams_list)} no new registration")
             print(tournament)
