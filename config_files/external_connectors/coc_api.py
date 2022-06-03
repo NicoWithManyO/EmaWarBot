7a9d681693e35c79f9cdc70e6497dd3561106b7f
NicoWithManyO <69017046+NicoWithManyO@users.noreply.github.com>
2022-06-04 00:14:33 +0200

    Delete coc_api.py

--- config_files/external_connectors/coc_api.py
+++
@@ -1,9 +0,0 @@
-
-import os
-import coc
-
-if os.environ.get("SC_API_LOGIN"):
-    coc_client = coc.login(os.environ.get("SC_API_LOGIN"), os.environ.get("SC_API_PASSWORD"), key_names = "ema_ewb", client = coc.EventsClient, key_count = 8)
-else:
-    import localtoken
-    coc_client = coc.login(localtoken.sc_id, localtoken.sc_pass, key_names="ewb_dev", key_count = 1)
